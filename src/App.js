c570df8d388181ace75c9cea4c6dbffbe638d5ec
Kathryn Patterson <katp@wegrok.net>
2018-10-04 17:15:07 -0400

    Beginning work - fixing styles

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Map from './Map';
+import Locations from './Locations'
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div>
+                <div class='container'>
+                    <Locations />
+                </div>
+                <div class='map' id='map'>
+                    <Map />;
+                </div>
+            </div>
+        ) // end of return statement
+    }  // end of render
+} // end of class
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -2,5 +2,7 @@ import React, { Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Map extends Component {
-    
-}
+
+  }
+
+export default Map;
